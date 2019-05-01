d9aa5fdc01c60d24d9c160ef261bd0acc56bb291
SL <wei.c@shulidata.com>
2019-05-01 11:34:40 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from 'pages/App.vue'
+import router from './routes'
+import { post, fetch, put } from './utils/http'
+import './plugins/element.js'
 
-Vue.config.productionTip = false
+Vue.prototype.$post = post;
+Vue.prototype.$fetch = fetch;
+Vue.prototype.$put = put;
+
 
 new Vue({
-  render: h => h(App),
+  router,
+  render(h) {
+    return h(App)
+  }
 }).$mount('#app')
